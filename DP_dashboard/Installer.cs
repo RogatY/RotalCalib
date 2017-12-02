960d7ece94ff487f6c952855057293639b4ce911
uri <roshtein@gmail.com>
2017-12-02 15:26:57 +0200

    save app.config during installation

--- DP_dashboard/Installer.cs
+++ DP_dashboard/Installer.cs
@@ -2,7 +2,6 @@
 using System.Collections;
 using System.ComponentModel;
 using System.IO;
-using System.Windows.Forms;
 
 namespace DP_dashboard
 {
@@ -14,47 +13,26 @@ namespace DP_dashboard
             InitializeComponent();
         }
 
-        public override void Install(System.Collections.IDictionary stateSaver)
+        protected override void OnBeforeInstall(IDictionary savedState)
         {
+            base.OnBeforeInstall(savedState);
             string path = this.Context.Parameters["targetdir"];
             if (File.Exists(path + "\\DP_dashboard.exe.config"))
             {
                 string Date = DateTime.Now.ToString("yyyy-MM-dd_HH-mm");
                 File.Copy(path + "\\DP_dashboard.exe.config", path + Date + "_DP_dashboard.exe.config" + ".bak");
             }
-            base.Install(stateSaver);
-        }
-
-        public override void Commit(IDictionary savedState)
-        {
-            string path = this.Context.Parameters["targetdir"];
-            if (File.Exists(path + "\\DP_dashboard.exe.config"))
-            {
-                string Date = DateTime.Now.ToString("yyyy-MM-dd_HH-mm");
-                File.Copy(path + "\\DP_dashboard.exe.config", path + Date + "_DP_dashboard.exe.config" + ".bak");
-            }
-            base.Commit(savedState);
-        }
-
-        public override void Rollback(IDictionary savedState)
-        {
-            base.Rollback(savedState);
         }
 
         public override void Uninstall(IDictionary savedState)
         {
-            
             string path = this.Context.Parameters["targetdir"];
             if (File.Exists(path + "\\DP_dashboard.exe.config"))
             {
-                DriveInfo di = new DriveInfo(path);
                 string Date = DateTime.Now.ToString("yyyy-MM-dd_HH-mm");
-
-                File.Copy(path + "\\DP_dashboard.exe.config", di.Name + Date + "__DP_dashboard.exe.config" + ".bak");
+                File.Copy(path + "\\DP_dashboard.exe.config", path + Date + "_DP_dashboard.exe.config" + ".bak");
             }
             base.Uninstall(savedState);
         }
-
-
     }
 }
